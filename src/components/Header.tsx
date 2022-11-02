421918d6eb0af0f801d7854239d97ccfe2ca16d1
KonstaKozh <konstakozh@yandex.ru>
2022-11-02 17:58:35 +0400

    create Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import logoUrl from '../img/Listek_logo_1.png'
+
+export const Header: any = () => {
+   return (
+      <div>
+         <div>
+            <img className="h-10 w-100" src={logoUrl} alt='logo_listek'/>
+         </div>
+
+      </div>
+   )
+}
